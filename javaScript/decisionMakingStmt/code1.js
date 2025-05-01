689bcc8390b1d8bf2ee67933347e9f764650993a
ganeshdutt <gdutt2304@gmail.com>
2025-05-01 17:55:08 +0530

    Decision making Statement

---
+++ javaScript/decisionMakingStmt/code1.js
@@ -0,0 +1,29 @@
+// let a = 4;
+// if (a >= 5) {
+//   console.log(a);
+// }
+// if (a >= 5) {
+//   console.log(true, a);
+// } else {
+//   console.log(false);
+// }
+
+// let userNumber = 6;
+
+// if (userNumber % 2 == 0) {
+//   console.log(userNumber, " : Even");
+// } else {
+//   console.log(userNumber, " : Odd");
+// }
+
+let studentMarks = 80;
+
+if (studentMarks >= 60) {
+  console.log("Pass with first Division");
+} else if (studentMarks >= 40) {
+  console.log("Pass with Second Division");
+} else if (studentMarks >= 30) {
+  console.log("Pass with third Division");
+} else {
+  console.log("Fail");
+}
