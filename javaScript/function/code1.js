94dc61d56a26e40c073c706ad6c08ab1c4f43dfd
ganeshdutt <gdutt2304@gmail.com>
2025-05-07 11:06:11 +0530

    Function

---
+++ javaScript/function/code1.js
@@ -0,0 +1,32 @@
+// let a = 56;
+// let b = 45;
+// let c = a + b;
+// console.log(c);
+
+// function sum(a, b) {
+//   return a + b;
+// }
+// console.log(sum(34, 87));
+// console.log(sum(4, 7));
+
+// let fun = function (a, b) {
+//   return a + b;
+// };
+
+// let output = fun(45, 98);
+
+// console.log(output);
+
+// iife
+// (function () {
+//   console.log("hello");
+// })();
+
+// arrowFunction
+// let arrowFunction = () => console.log("Hello ");
+// let arrowFunction1 = () => {
+//   console.log("Hello ");
+//   console.log("world ");
+// };
+
+// arrowFunction1();
