d3fb40e1e03b178f5f9788bea28e1b19b78c1a91
ganeshdutt <gdutt2304@gmail.com>
2025-05-07 17:35:50 +0530

    Call Method

--- javaScript/function/code2.js
+++ javaScript/function/code2.js
@@ -9,23 +9,39 @@
 
 // fun.call(null, "Ganesh Dutt");
 
-function Emp(id, name) {
-  this.id = id;
-  this.name = name;
-}
+// function Emp(id, name) {
+//   this.id = id;
+//   this.name = name;
+// }
 
-function PermanentEmp(id, name, salary) {
-  this.salary = salary;
-  Emp.call(this, id, name);
-}
-function TemporaryEmp(id, name, salary) {
-  PermanentEmp.call(this, id, name, salary);
-}
+// function PermanentEmp(id, name, salary) {
+//   this.salary = salary;
+//   Emp.call(this, id, name);
+// }
+// function TemporaryEmp(id, name, salary) {
+//   PermanentEmp.call(this, id, name, salary);
+// }
 
-let emp1 = new PermanentEmp(1, "Nitesh", 3287387);
-let emp2 = new TemporaryEmp(2, "Babul", 832738);
+// let emp1 = new PermanentEmp(1, "Nitesh", 3287387);
+// let emp2 = new TemporaryEmp(2, "Babul", 832738);
 
-console.log(emp1);
-console.log(emp2);
+// console.log(emp1);
+// console.log(emp2);
 
 // this.nitesh  = nitesh
+
+const person1 = {
+  name: "Ganesh",
+};
+const person2 = {
+  name: "Rahul",
+};
+
+function greet(city, country) {
+  console.log(
+    "Hello my name is " + this.name + " I am from " + city + " in " + country
+  );
+}
+
+greet.call(person1, "Noida", "India");
+greet.call(person2, "Delhi", "India");
