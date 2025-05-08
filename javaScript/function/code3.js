eeeabf31739b69856a72bb3eba2e10bde8b58bd9
ganeshdutt <gdutt2304@gmail.com>
2025-05-08 18:28:12 +0530

    Loop

--- javaScript/function/code3.js
+++ javaScript/function/code3.js
@@ -9,21 +9,50 @@ document.writeln("Bind method ");
 // let a = kuchBhi.bind(null, "John");
 // console.log(a);
 
-let website = {
-  name: "javaScript",
-  getName: function () {
-    return this.name;
-  },
-};
+// let website = {
+//   name: "javaScript",
+//   getName: function () {
+//     return this.name;
+//   },
+// };
 
-let changeName = {
-  name: "Node.js",
-};
+// let changeName = {
+//   name: "React",
+// };
 
 // let output = website.getName();
 // console.log(output);
 
-let unboundGetName = website.getName;
-let boundGetName = unboundGetName.bind(changeName);
+// let unboundGetName = website.getName;
+// let boundGetName = unboundGetName.bind(changeName);
 
-console.log(boundGetName());
+// console.log(boundGetName());
+
+// let person = {
+//   name: "",
+//   age: 30,
+// };
+
+// console.log(person.name);
+
+// let person = {
+//   name: "Ganesh Dutt",
+// };
+// function sayHello() {
+//   console.log("Hello, " + this.name);
+// }
+// let output = sayHello.bind(person);
+// output();
+
+// call    apply  bind
+// yes      yes    no    - calls function immediately
+// individually     in an Array   individually  - pass Arguments
+// no       no      yes
+
+document.writeln("toString()");
+
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+// console.log(typeof add(10, 50));
+// console.log(typeof add(10, 50).toString());
---
+++ javaScript/loop/code1.js
@@ -0,0 +1,32 @@
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+//  let value  = 9;
+//  if(value >= 5){
+//     console.log("value is greater than or equal to 5");
+//  }
+//    value++
+//   10
+// for (let value = 2; value <= 10; value++) {
+//   console.log(value + " Ganesh Dutt ");
+// }
+// 1 to 10
+// let i;
+
+// for (i = 1; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     console.log("Even " + i);
+//   } else {
+//     console.log("odd " + i);
+//   }
+// }
+
+// table
+let table = 5;
+
+for (let i = 1; i <= 10; i++) {
+  console.log(table + " * " + i + " = " + table * i);
+}
+
+// 5 *  1 = 5
